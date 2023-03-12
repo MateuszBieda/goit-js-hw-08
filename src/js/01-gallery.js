bf5e89472aee4a4171fe99487a99ba870d19e48c
MateuszBieda <mateusbieda@gmail.com>
2023-03-12 22:08:37 +0100

    task_1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,3 +5,22 @@ import { galleryItems } from './gallery-items';
 console.log(galleryItems);
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+const list = galleryItems.map((image)=> 
+`<div class="gallery__item">
+  <a class="gallery__link" href=${image.original}>
+    <img
+      class="gallery__image"
+      src=${image.preview}
+      data-source=${image.original}
+      alt="${image.description}"
+    />
+  </a>
+</div>`).join(" ");
+
+//console.log(galeryItems);
+gallery.insertAdjacentHTML('beforeend', list);
+
+let galleryList=  new SimpleLightbox('.gallery a',{captions: true, captionDelay:250, captionPosition:'bottom',captionType:'attr', captionsData:'alt',captionSelector:'img' } );
+galleryList.next();
