65ce5ad3ddd7a0a7b63c865bc192e0d24a337af7
MateuszBieda <mateusbieda@gmail.com>
2023-03-13 20:10:34 +0100

    dddd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const STORAGE_TIME_KEY = 'videoplayer-current-time';
+
+player.on("timeupdate", throttle(onPlay, 1000));
+
+function onPlay(e) {
+    localStorage.setItem(STORAGE_TIME_KEY, e.seconds);
+  }
+player.setCurrentTime(JSON.parse(localStorage.getItem(STORAGE_TIME_KEY))||0);
+
+
+
+
